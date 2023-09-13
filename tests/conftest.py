da534dc76863b389f187f4ab260415c4fbce4935
Rodolphe R. CLEMENCEAU <RCLEMENCEAU@slts-asp.local>
2023-09-13 19:48:13 +0200

    Correctif pour ne pas réserver sur une compétiotion dans le passé

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,11 +10,32 @@ def client():
         yield client
 
 
-clubs_data = [{'name': 'Test Club 1', 'email': 'test@test.com', 'points': '12'},
-              {'name': 'Test Club 2', 'email': 'test2@test.com', 'points': '13'}]
+clubs_data = [
+        {
+            'name': 'Test Club 1',
+            'email': 'test@test.com',
+            'points': '12'
+        },
+        {
+            'name': 'Test Club 2',
+            'email': 'test2@test.com',
+            'points': '13'
+        },
+        {
+            "name": "Simply Lift",
+            "email": "john@simplylift.co",
+            "points": "13"
+        }
+    ]
 
 competitions_data = [{
     "name": "Competition Test",
     "date": "2023-10-10 10:10:10",
     "numberOfPlaces": "25",
-}]
+    },
+    {
+    "name": "Competition_Test_date",
+    "date": "2020-10-10 10:10:10",
+    "numberOfPlaces": "25",
+    }
+]
